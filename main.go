4d53b3129af062ddcd5255e4419556313a73ac59
leeforster28 <leeforster@techniche.com.au>
2023-02-07 08:28:58 +1100

    First commit

--- main.go
+++ main.go
@@ -1,54 +1,148 @@
 package main
 
-// these are the packages we are importing
-// if you import it you gotta use it
 import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
+	"io"
+	"net"
 	"net/smtp"
 	"os"
+	"strings"
+	"text/template"
 
-	"github.com/joho/godotenv"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
+type Request struct {
+	Sender   string `json:"sender"`
+	Receiver string `json:"receiver"`
+	Message  string `json:"message"`
+	URL      string `json:"url"`
+}
 
-	// load .env file from given path
-	// we keep it empty it will load .env from current directory
-	err := godotenv.Load(".env")
+type loginAuth struct {
+	username, password string
+}
 
+func LoginAuth(username, password string) smtp.Auth {
+	return &loginAuth{username, password}
+}
+
+func (a *loginAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	return "LOGIN", []byte(a.username), nil
+}
+
+func (a *loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		switch string(fromServer) {
+		case "Username:":
+			return []byte(a.username), nil
+		case "Password:":
+			return []byte(a.password), nil
+		default:
+			return nil, errors.New("unknown from server")
+		}
+	}
+	return nil, nil
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	bodyReader := strings.NewReader(request.Body)
+	body, err := io.ReadAll(bodyReader)
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		return events.APIGatewayProxyResponse{
+			Body:       "Failed at reading body",
+			StatusCode: 500,
+		}, nil
 	}
 
-	// Sender data.
-	// Get these env variables from your .env file
+	var req Request
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
+	}
+	if req.Sender == "" || req.Receiver == "" || req.Message == "" || req.URL == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "Missing fields",
+			StatusCode: 400,
+		}, nil
+	}
+
 	from := os.Getenv("EMAIL")
-	password := os.Getenv("EMAIL_PASSWORD")
+	password := os.Getenv("PASSWORD")
 
-	// Receiver email address.
-	// replace my email address with yours unless if you want to send me an email :)
 	to := []string{
-		"toshvelaga@gmail.com",
+		req.Receiver,
 	}
 
-	// smtp server configuration.
-	smtpHost := "smtp.gmail.com"
+	smtpHost := "smtp.office365.com"
 	smtpPort := "587"
 
-	// Message.
-	message := []byte(
-		"Subject: This is a test email\r\n" + "\r\n" + "Here is a test message\r\n")
-
-	// Authentication.
-	auth := smtp.PlainAuth("", from, password, smtpHost)
-
-	// Sending email.
-	emailErr := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if emailErr != nil {
-		fmt.Println(emailErr)
-		return
+	conn, err := net.Dial("tcp", "smtp.office365.com:587")
+	if err != nil {
+		println(err)
 	}
 
-	fmt.Printf("Email successfully sent to %s", to[0])
+	c, err := smtp.NewClient(conn, smtpHost)
+	if err != nil {
+		println(err)
+	}
+
+	tlsconfig := &tls.Config{
+		ServerName: smtpHost,
+	}
+
+	if err = c.StartTLS(tlsconfig); err != nil {
+		println(err)
+	}
+
+	auth := LoginAuth(from, password)
+
+	if err = c.Auth(auth); err != nil {
+		println(err)
+	}
+
+	t, _ := template.ParseFiles("template.html")
+
+	var emailBody bytes.Buffer
+
+	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	emailBody.Write([]byte(fmt.Sprintf("Subject: Tecsim Notification \n%s\n\n", mimeHeaders)))
+
+	t.Execute(&emailBody, struct {
+		Sender  string
+		Message string
+		URL     string
+	}{
+		Sender:  req.Sender,
+		Message: req.Message,
+		URL:     req.URL,
+	})
+
+	// Sending email.
+	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, emailBody.Bytes())
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			Body:       "Failed at sending email",
+			StatusCode: 500,
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "Email sent",
+		StatusCode: 200,
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(Handler)
 }
